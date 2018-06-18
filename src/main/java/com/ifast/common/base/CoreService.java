d314d0c4eec6c1ec12710d88228631e4edba1fd2
Aron <izenglong@163.com>
2018-06-18 16:45:29 +0800

    增加骚查询方法

--- src/main/java/com/ifast/common/base/CoreService.java
+++ src/main/java/com/ifast/common/base/CoreService.java
@@ -1,6 +1,10 @@
 package com.ifast.common.base;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.service.IService;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <pre>
@@ -12,5 +16,33 @@ import com.baomidou.mybatisplus.service.IService;
  * @param <T>
  */
 public interface CoreService<T> extends IService<T> {
+    List<T> findByKv(Object... param);
+
+    T findOneByKv(Object... param);
+
+    /**
+     * <pre>
+     *
+     * </pre>
+     *
+     * <small> 2018/6/14 17:32 | Aron</small>
+     * @param [clazz, param]
+     * @return java.util.Map<java.lang.String,java.lang.Object>
+     *
+     */
+
+    Map<String, Object> convertToMap(Object... param);
+
+    /**
+     * <pre>
+     *
+     * </pre>
+     *
+     * <small> 2018/6/14 17:14 | Aron</small>
+     * @param [clazz, params]
+     * @return java.util.Map<java.lang.String,java.lang.Object>
+     *
+     */
+    EntityWrapper<T> convertToEntityWrapper(Object... params);
 
 }
--- src/main/java/com/ifast/common/base/CoreServiceImpl.java
+++ src/main/java/com/ifast/common/base/CoreServiceImpl.java
@@ -1,21 +1,68 @@
 package com.ifast.common.base;
 
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <pre>
  * 通用业务层实现
  * </pre>
- * 
+ *
  * <small> 2018年1月9日 | Aron</small>
- * 
- * @param <M> mapper
- * @param <T> entity
+ *
+ * @param <M>
+ *            mapper
+ * @param <T>
+ *            entity
  */
 public abstract class CoreServiceImpl<M extends BaseMapper<T>, T> extends ServiceImpl<M, T> implements CoreService<T> {
     public Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public List<T> findByKv(Object... params) {
+        if (params == null) {
+            return new ArrayList<>();
+        }
+        return baseMapper.selectByMap(convertToMap(params));
+    }
+
+    @Override
+    public T findOneByKv(Object... params) {
+        return selectOne(this.convertToEntityWrapper(params));
+    }
+
+    @Override
+    public Map<String, Object> convertToMap(Object... params) {
+        Map<String, Object> map = new HashMap<>();
+        if (params == null) {
+            return map;
+        }
+        for (int i = 0; i < params.length; i++) {
+            if (i % 2 == 1) {
+                map.put((String) params[i - 1], params[i]);
+            }
+        }
+        return map;
+    }
+    @Override
+    public EntityWrapper<T> convertToEntityWrapper(Object... params) {
+        EntityWrapper<T> ew = new EntityWrapper<>();
+        if (params == null) {
+            return ew;
+        }
+        for (int i = 0; i < params.length; i++) {
+            if (i % 2 == 1) {
+                ew.eq((String) params[i - 1], params[i]);
+            }
+        }
+        return ew;
+    }
 }
