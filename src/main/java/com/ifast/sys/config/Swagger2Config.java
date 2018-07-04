f1a76694dfd3c94117a298ed176536f37c7530ea
xlongwei <admin@xlongwei.com>
2018-07-05 00:18:24 +0800

    配置swagger的host，适用于nginx反向代理

--- src/main/java/com/ifast/sys/config/Swagger2Config.java
+++ src/main/java/com/ifast/sys/config/Swagger2Config.java
@@ -2,6 +2,10 @@ package com.ifast.sys.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.ifast.common.config.IFastConfig;
+import com.ifast.common.utils.SpringContextHolder;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -17,7 +21,11 @@ public class Swagger2Config {
 
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+    	IFastConfig ifastConfig = SpringContextHolder.getBean(IFastConfig.class);
+    	String projectRootURL = ifastConfig.getProjectRootURL();
+    	int s = projectRootURL.indexOf("//"), e = projectRootURL.indexOf('/', s+2);
+    	String host = projectRootURL.substring(s+2, e);
+        return new Docket(DocumentationType.SWAGGER_2).host(host).apiInfo(apiInfo()).select()
                 // 为当前包路径
                 .apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
     }
