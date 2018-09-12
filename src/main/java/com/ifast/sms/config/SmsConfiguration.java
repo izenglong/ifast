6f0bf30e7bfa8dc72282581c7ed18658472da3ae
xlongwei <admin@xlongwei.com>
2018-09-12 15:17:52 +0800

    阿里云短信发送

--- src/main/java/com/ifast/sms/config/SmsConfiguration.java
+++ src/main/java/com/ifast/sms/config/SmsConfiguration.java
@@ -1,11 +1,13 @@
 package com.ifast.sms.config;
 
 import com.ifast.common.config.CacheConfiguration;
+import com.ifast.sms.sender.AliyunSender;
 import com.ifast.sms.sender.ZhuTongSender;
 import com.ifast.sms.support.SmsManager;
 import com.ifast.sms.support.SmsSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.Cache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,12 +30,23 @@ public class SmsConfiguration {
     }
 
     @Bean
+    @ConditionalOnProperty(prefix="ifast.sms.zt", name="passwd", matchIfMissing=false)
     SmsSender zhuTongSender(ZhuTongProperties properties){
         if(log.isDebugEnabled()){
             log.debug("启用上海助通短信服务");
         }
         SmsSender sender = new ZhuTongSender(properties);
         return sender;
+    }
+    
+    @Bean
+    @ConditionalOnProperty(prefix="ifast.sms.aliyun", name="accessKeySecret", matchIfMissing=false)
+    SmsSender aliyunSender(AliyunProperties properties){
+    	if(log.isDebugEnabled()){
+    		log.debug("启用阿里云短信服务");
+    	}
+    	SmsSender sender = new AliyunSender(properties);
+    	return sender;
     }
 
 }
