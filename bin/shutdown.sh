5dd5575a10d9ec2a9d7f73389ae698aac181886f
Aron <izenglong@163.com>
2019-05-10 12:52:46 +0800

    :bug: 启动脚本

--- bin/shutdown.sh
+++ bin/shutdown.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 jarName=$1
-echo " ---  "
+if [ -z "${jarName}" ]
+then
+    jarName=ifast-1.0.0.jar
+fi
 ps -ef | grep ${jarName} | grep -v grep | grep -v deploy | awk '{print $2}' | xargs kill -9
--- bin/startup.sh
+++ bin/startup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 jarName=$1
+if [ -z "${jarName}" ]
+then
+    jarName=ifast-1.0.0.jar
+fi
 profile=$2
+if [ -z "${profile}" ]
+then
+    profile=dev
+fi
 
 nohup java -jar ../${jarName} --spring.profiles.active=${profile} >/dev/null 2>&1 &
