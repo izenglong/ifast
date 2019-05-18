4a61337c20a987010cbbec927c64997a1d26e8b3
Aron <izenglong@163.com>
2019-05-18 22:36:01 +0800

    :white_check_mark: sql更新

--- ifast.sql
+++ ifast.sql
@@ -317,7 +317,7 @@ CREATE TABLE `sys_user` (
 BEGIN;
 INSERT INTO `sys_user` VALUES ('1', 'admin','6fb30533944d4d0682ae24e45dc772f1', '超级管理员', '92898fba2d9bdafe0f44dcace5082deff13eba85ba633f4299141a661103aac4', '6', 'izenglong@163.com', '15277778888', '1', '1', '2017-08-15 21:40:39', '2017-08-15 21:41:00', '96', '2018-04-02 00:00:00', '151', '创客基地', '', '广东省', '广州市', '番禺区'),
                               ('2', 'test','6fb30533944d4d0682ae24e45dc772f1', '临时用户', '92898fba2d9bdafe0f44dcace5082deff13eba85ba633f4299141a661103aac4', '6', 'test@ifast.com', '15278792752', '1', '1', '2017-08-14 13:43:05', '2017-08-14 21:15:36', '96', '2018-08-22 00:00:00', null, '', '', '北京市', '北京市市辖区', '东城区'),
-                              ('1034088697579159554', 'appUser','6fb30533944d4d0682ae24e45dc772f1' 'user', '92898fba2d9bdafe0f44dcace5082deff13eba85ba633f4299141a661103aac4', '12', 'appUser@ifast.com', null, '1', null, null, null, null, null, null, null, null, null, null, null);
+                              ('1034088697579159554', 'appUser','6fb30533944d4d0682ae24e45dc772f1', 'user', '92898fba2d9bdafe0f44dcace5082deff13eba85ba633f4299141a661103aac4', '12', 'appUser@ifast.com', null, '1', null, null, null, null, null, null, null, null, null, null, null);
 COMMIT;
 
 -- ----------------------------
